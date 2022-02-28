1cf0c87c6ef7fb7ad54e28cd8d485eb33b869ca6
huaj-todd <toddhua1994@gmail.com>
2022-02-27 22:32:01 -0500

    a5 finished navigation side bar

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -5,14 +5,17 @@ import NavigationSidebar from "../NavigationSidebar/index.js";
     $('#wd-explore').append(`
 <div class="row mt-2">
 <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-${NavigationSidebar()}
+ ${NavigationSidebar()}
 </div>
+
 <div class="col">
-${ExploreComponent()}
+<span>Second</span>
 </div>
-//<div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-${WhoToFollowList()}
+
+<div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+<span>Third</span>
 </div>
+
 </div>
 `);
 })($);
--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -1,14 +1,39 @@
 const NavigationSidebar = () => {
     return(`
 <div class="list-group">
-<a class="list-group-item" href="/">
-<i class="fab fa-twitter"></i></a>
+    <a class="list-group-item list-group-item-action" href="/"style="background-color: black">
+    <i class="fab fa-twitter" style="color: white"></i>
+    </a>
+    <a class="list-group-item list-group-item-action"style="background-color: black">
+    <i class="fas fa-house-user" style="color: white">Home</i>
+    </a>
+    <a class="list-group-item list-group-item-action active " style="background-color: #0a53be">
+     <i class="fas fa-hashtag" style="color: white">Explore</i>
+     </a>
+     <a class="list-group-item list-group-item-action" style="background-color: black">
+                <i class="fas fa-bell" style="color: white">Notification</i>
+      </a>
+      <a class="list-group-item list-group-item-action"style="background-color: black">
+                <i class="fas fa-envelope" style="color: white">Message</i>
+      </a>
+      <a class="list-group-item list-group-item-action"style="background-color: black">
+                <i class="fas fa-bookmark"style="color: white">Bookmark</i>
+      </a>
+      <a class="list-group-item list-group-item-action"style="background-color: black">
+                <i class="fas fa-list"style="color: white">List</i>
+      </a>
+      <a class="list-group-item list-group-item-action"style="background-color: black">
+                <i class="fas fa-user"style="color: white">Profile</i>
+      </a>
+      <a class="list-group-item list-group-item-action"style="background-color: black">
+                <i class="fas fa-circle-ellipsis"style="color: white">More</i>
+      </a>
 <!-- continue the rest of the list -->
 </div>
 <div class="d-grid mt-2">
-<a href="tweet.html"
-class="btn btn-primary btn-block rounded-pill">
-Tweet</a>
+    <a href="tweet.html"
+    class="btn btn-primary btn-block rounded-pill" style="background-color: #0a53be">
+    Tweet</a>
 </div>
 `);
 }
