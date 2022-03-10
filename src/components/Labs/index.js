36f2e7748ef7cdccda8ede939d84d9f401124789
huaj-todd <toddhua1994@gmail.com>
2022-03-10 14:28:47 -0500

    A6 labs finish done before tolist labs

--- src/components/Labs/index.js
+++ src/components/Labs/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from  "./ConditionalOutput";
 const Labs = () => {
     return(
-        <>
+        <div>
             <h1>Labs</h1>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
             <Link to="/hello">
                 Hello
             </Link> |
             <Link to="/tuiter">
                 Tuiter
             </Link>
-        </>
+        </div>
     )
 };
 export default Labs;
