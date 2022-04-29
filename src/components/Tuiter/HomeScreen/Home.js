1fe2d9e32810a65f1f39f974d849b0d434eb9e81
huaj-todd <toddhua1994@gmail.com>
2022-04-29 18:49:53 -0400

    something

--- src/components/Tuiter/HomeScreen/Home.js
+++ src/components/Tuiter/HomeScreen/Home.js
@@ -2,17 +2,21 @@ import NavigationSidebar from "../NavigationSidebar/index.js";
 import React from "react";
 import PostList from "../PostList/index.js";
 import PostSummaryList from "../PostSummaryList/index.js";
+import WhoToFollowList from "../WhoToFollowList/index"
 const HomeScreen = () => {
     return(
         <>
             <div class="row mt-2">
-            <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-             {NavigationSidebar()}
+            <div class="col-2">
+             <NavigationSidebar />
             </div>
 
-            <div class="col">
-            {PostList()}
+            <div class="col-7">
+            <PostList/>
             </div>
+                <div className="col-3">
+                    <WhoToFollowList/>
+                </div>
 
             </div>
         </>
