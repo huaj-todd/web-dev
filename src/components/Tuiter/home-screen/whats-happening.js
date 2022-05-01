ee7afbc017453a716b7f1d6832b2f84874b4f738
huaj-todd <toddhua1994@gmail.com>
2022-05-01 17:14:23 -0400

    something

--- src/components/Tuiter/home-screen/whats-happening.js
+++ src/components/Tuiter/home-screen/whats-happening.js
@@ -10,10 +10,12 @@ const WhatsHappening = () => {
         <>
             <div className="pt-2 row">
                 <div className="col-1">
-                    <img className="wd-rounded-corners" height="50" width="50" src="../../../images/node.png"/>
+                    <img className="wd-rounded-corners" height="50" width="50" src="tuiters/avatar.jpg"/>
                 </div>
                 <div className="col-9">
-                    <textarea className="form-control w-75 bg-black text-white ms-3 w-100 wd-search-area" onChange={(e) => setNewTuit({...newTuit, tuit: e.target.value})}/>
+                    <textarea className="form-control w-75 bg-black text-white ms-3 w-100 wd-search-area" onChange={(
+                        e) =>
+                        setNewTuit({...newTuit, tuit: e.target.value})}/>
                    <hr className="ms-3" size="4"/>
                     <i className="fas fa-image text-primary wd-icon-margin ms-3" ></i>
                     <i className="fas fa-chart-line text-primary wd-icon-margin"></i>
